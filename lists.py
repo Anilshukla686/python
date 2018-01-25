f0cbba1ea2eff7cea3897c2e137db7c3469aed81
anil686 <anilshukla686@gmail.com>
2018-01-25 12:22:57 +0530

    mesg

---
+++ lists.py
@@ -0,0 +1,15 @@
+city=["Mumbai","Delhi","Jalandhar","Kolkata"]
+print(city)
+print(city+["Bhopal"])
+for i in reversed(city):
+    print(i)
+empty=[]
+letters=['a','b','c','d','e']
+numbers=[1,2,3,4,5]
+mixed=[1,2,"Anil",44]
+print(city[-1])
+x=[letters,numbers]
+print(x)
+print(len(x))
+print(x[0][1])
+print('a' in 'Anil')
